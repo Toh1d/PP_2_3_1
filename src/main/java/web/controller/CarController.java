ffcb40bfdde103c642544c0be44a4f2c5b35d513
Tohid <tokhid.kurbanov@gmail.com>
2023-04-14 15:22:40 +0300

    исправил метод getCars в CarContoller

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
 import web.service.CarService;
+
+import java.util.List;
 
 @Controller
 public class CarController {
@@ -15,8 +18,8 @@ public class CarController {
     }
 
     @GetMapping(value = "/cars")
-    public String getCars(@RequestParam(value = "count", required = false) int count,
-                            Model model) {
+    public String getCars(@RequestParam(value = "count", defaultValue = "5") int count,
+                          Model model) {
         model.addAttribute("cars", carService.getCarsList(count));
         return "cars";
     }
